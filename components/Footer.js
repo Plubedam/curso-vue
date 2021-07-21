4238444f3b812115d0ecbc6d4255f4c81c8ec402
Plubedam <pedroluisubedam@hotmail.com>
2021-07-21 09:29:19 +0200

    feat: añade props cantidad y fecha en Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,14 @@
 // Nombre de componente siempre en minuscula y separado por "-"
 app.component('footer-banco', {
+    props: ['cantidad', 'fecha'],
     template: /*html*/ `<div class="bg-dark py-3 mt-2 text-white">
-                            <h3>Footer de mi sitio web</h3>
+                            <h3>{{texto}} - {{cantidad}}</h3>
+                            <p class="p-2">{{fecha}}</p>
                         </div>` 
+    ,
+    data(){
+        return{
+            texto: "Footer de mi sitio web dinámico"
+        }
+    }
 })
