323aaa6cdff0e52f2a2ae412eb5dabb65162e927
Plubedam <pedroluisubedam@hotmail.com>
2021-07-21 08:56:44 +0200

    fix: añade el componente Footer

---
+++ components/Footer.js
@@ -0,0 +1,6 @@
+// Nombre de componente siempre en minuscula y separado por "-"
+app.component('footer-banco', {
+    template: /*html*/ `<div class="bg-dark py-3 mt-2 text-white">
+                            <h3>Footer de mi sitio web</h3>
+                        </div>` 
+})
