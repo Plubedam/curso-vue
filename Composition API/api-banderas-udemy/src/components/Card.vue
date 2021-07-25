7c16dbf9617085602c6ac4671a8c6cae63d780f9
Plubedam <pedroluisubedam@hotmail.com>
2021-07-25 20:06:37 +0200

    feat: add list of countries with info

---
+++ Composition API/api-banderas-udemy/src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card mt-3">
+      <div class="card-body">
+          <h4 class="text-center">{{pais.name}}</h4>
+          <p class="text-center">
+              <img :src="`${pais.flag}`" :alt="`bandera-${pais.name}`" class="img-fluid w-50">
+          </p>
+          <p class="card-text">
+              <span class="badge badge-dark d-block mb-1">Nombre nativo: {{pais.nativeName}}</span>
+              <span class="badge badge-info p-3 d-block mb-1">Población: {{numeroFormato(pais.population)}}</span>
+              <span class="badge badge-warning d-block mb-1">Capital: {{pais.capital}}</span>
+              <span class="badge badge-success d-block mb-1">Continente: {{pais.region}}</span>
+
+
+          </p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['pais'],
+    setup(){
+        const numeroFormato = (num) => {
+            return Intl.NumberFormat("de-DE").format(num)
+        }
+
+        return {numeroFormato}
+    }
+}
+</script> 
--- Composition API/api-banderas-udemy/src/components/CardList.vue
+++ Composition API/api-banderas-udemy/src/components/CardList.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="row">
-      <div class="col-12">
-          card
+      <div 
+      class="col-12"
+      v-for="(pais) in paises" :key="pais.name"
+      >
+        <Card :pais="pais"/>  
       </div>
   </div>
 </template>
 
 <script>
-import { onMounted } from '@vue/runtime-core'
+import { computed, onMounted } from '@vue/runtime-core'
 import {useStore} from 'vuex'
+import Card from './Card'
 
 export default {
+    components:{
+        Card
+    },
     setup(){
         const store = useStore()
+
+        const paises = computed(() => {
+            return store.getters.topPaisesPoblacion
+        }) 
 
         onMounted(() => {
             store.dispatch('getPaises')
         })
 
-        return {}
+        return {paises}
     }
 }
 </script>
