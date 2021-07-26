30e3984a48575763a39380ace206fc18f697daca
Plubedam <pedroluisubedam@hotmail.com>
2021-07-26 09:30:46 +0200

    feat: adds fundamentals provide-inject app

---
+++ Composition API/provide-app/src/App.vue
@@ -0,0 +1,28 @@
+<template> 
+  <h1>App contador: {{contador}}</h1>
+  <contador-uno/>
+  <contador-accion />
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import ContadorUno from './components/ContadorUno.vue'
+import ContadorAccion from './components/ContadorAccion.vue'
+
+import { provide } from '@vue/runtime-core'
+
+export default {
+  name: 'App',
+  components: {
+    ContadorUno, ContadorAccion
+  },
+  setup(){
+    const contador = ref(2)
+
+    // Provide
+    provide('contador', contador)
+
+    return {contador}
+  }
+}
+</script> 
